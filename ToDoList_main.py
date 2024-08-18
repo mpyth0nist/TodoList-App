ce3432d49646417c60fe0143ac7f9018381da256
Oussama.py <devossama9@gmail.com>
2024-08-18 17:11:53 +0100

    Added the update tasks option

--- ToDoList_main.py
+++ ToDoList_main.py
@@ -2,16 +2,28 @@ import csv
 
 
 # The main todo list class
+
+
 class TodoList():
     
-    
+
     def __init__(self):
         # setting the default list header
+        self.task_num = 1
         self.tasks = [['Task', 'Priority']]
 
     # Add a task
     def set_task(self,task, priority):
-        self.tasks.append(f"{task}{priority}")
+        self.tasks.append([self.task_num,task,priority])
+        self.task_num+=1
+
+    # Update a task.
+    def update_task(self, task_num, new_task):
+        # Search for the task in the todo list
+        for i in range(1,len(self.tasks)):
+                # Replace it if found in the list
+            if self.tasks[i][0] == task_num:
+                self.tasks[i][1] = new_task
 
     # Remove a task
     def del_task(self, task):
@@ -22,44 +34,30 @@ class TodoList():
         if len(self.tasks) < 2:
             print("Your ToDo List is empty, Try adding some Tasks to it!")
         else:
-            sort_tasks(self.tasks)
-            printable = remove_indexing(self.tasks)
-            print(printable)
+            print(self.tasks)
 
     # Return the Todo list as a value
     def get_list(self):
         if len(self.tasks) < 2:
             return "Your list is empty."
-        return remove_indexing(sort_tasks(self.tasks))
+        return self.tasks
 
-
-# Sort task per priority
-def sort_tasks(tasks):
-    for i in range(1,len(tasks)-1):
-        if int(tasks[i][-1]) > int(tasks[i+1][-1]):
-            tasks[i],tasks[i+1] = tasks[i+1], tasks[i]
-
-
-# Removes the priority indexing at the end of each task
-def remove_indexing(tasks):
-    cleared = tasks.copy()
-    for i in range(1,len(cleared)):
-        cleared[i] = cleared[i].replace(cleared[i][-1], "")
-
-    return cleared
 
 
 
 print("Welcome to Tasky! A simple Minimalistic Todo-List App. \n This mini-program created a todolist for you and save it as a CSV file that you can use after.")
-print("[A]: Add a Task.  \n [U]: Update a Task.  \n [S]: Show Tasks. \n [Q]: Quit the App.")
 
-choice = input('You choose to .. : ')
 
 # Initializing a Todolist instance
 td = TodoList()
 
+choice = ''
 # Main loop of the program.
 while choice != 'Q':
+
+    print("[A]: Add a Task.  \n [U]: Update a Task.  \n [S]: Show Tasks. \n [Q]: Quit the App.")
+
+    choice = input('You choose to .. : ')
     
     # If the user chooses to Add a task.
     if choice.upper() == 'A':
@@ -70,11 +68,38 @@ while choice != 'Q':
             priority = input("Task priority[1: Crucial, 2: Important, 3: Optional]: ")
 
             td.set_task(task,int(priority))
-            
+
             quit = input("Press q to quit or press any other key to add more tasks: ")
 
             if quit == 'q':
                 break
+
+    if choice.upper() == 'U':
+        while True:
+            
+            # Getting the task number
+            user_input = input("Which task you want to update? \n[ If you forgot the task number, You can list all tasks to choose which one by typing 's' ]: ")
+            
+            if user_input.upper() == 'S':
+                print("[TASK_NUMBER, TASK, PRIORITY]")
+                print(td.get_list()[1:])
+                continue
+            # converting the input into an int
+            
+            else:
+                task_num = int(user_input)
+
+                # Getting the new task
+                new_task = input("Enter the new task: ")
+
+                # Updating the task
+                td.update_task(task_num, new_task)
+
+                # Printing the Updated ToDo list
+                print(td.get_list())
+
+
+
 
 
 
