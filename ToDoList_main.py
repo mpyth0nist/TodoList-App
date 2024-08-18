604b74b1c9665ff20864697bc46e9ac06970e7ce
Oussama.py <devossama9@gmail.com>
2024-08-18 16:25:22 +0100

    adding some comments

--- ToDoList_main.py
+++ ToDoList_main.py
@@ -1,16 +1,23 @@
 import csv
 
+
+# The main todo list class
 class TodoList():
     
+    
     def __init__(self):
+        # setting the default list header
         self.tasks = [['Task', 'Priority']]
 
+    # Add a task
     def set_task(self,task, priority):
         self.tasks.append(f"{task}{priority}")
 
+    # Remove a task
     def del_task(self, task):
         self.tasks.remove(task)
 
+    # Print the full Todo list
     def show_list(self):
         if len(self.tasks) < 2:
             print("Your ToDo List is empty, Try adding some Tasks to it!")
@@ -19,18 +26,21 @@ class TodoList():
             printable = remove_indexing(self.tasks)
             print(printable)
 
+    # Return the Todo list as a value
     def get_list(self):
         if len(self.tasks) < 2:
             return "Your list is empty."
         return remove_indexing(sort_tasks(self.tasks))
 
 
+# Sort task per priority
 def sort_tasks(tasks):
     for i in range(1,len(tasks)-1):
         if int(tasks[i][-1]) > int(tasks[i+1][-1]):
             tasks[i],tasks[i+1] = tasks[i+1], tasks[i]
 
 
+# Removes the priority indexing at the end of each task
 def remove_indexing(tasks):
     cleared = tasks.copy()
     for i in range(1,len(cleared)):
@@ -40,15 +50,18 @@ def remove_indexing(tasks):
 
 
 
-lists = {}
 print("Welcome to Tasky! A simple Minimalistic Todo-List App. \n This mini-program created a todolist for you and save it as a CSV file that you can use after.")
 print("[A]: Add a Task.  \n [U]: Update a Task.  \n [S]: Show Tasks. \n [Q]: Quit the App.")
+
 choice = input('You choose to .. : ')
 
+# Initializing a Todolist instance
 td = TodoList()
 
-
+# Main loop of the program.
 while choice != 'Q':
+    
+    # If the user chooses to Add a task.
     if choice.upper() == 'A':
         while True:
 
@@ -57,7 +70,7 @@ while choice != 'Q':
             priority = input("Task priority[1: Crucial, 2: Important, 3: Optional]: ")
 
             td.set_task(task,int(priority))
-
+            
             quit = input("Press q to quit or press any other key to add more tasks: ")
 
             if quit == 'q':
